ca19c700e27d466ff6ef016978db85988ca6bb70
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2022-01-08 01:32:32 +0530

    Checking for Duplicate Contact in AddressBook

--- Address-Book-System-Javascript/Address-Book-System.js
+++ Address-Book-System-Javascript/Address-Book-System.js
@@ -1,5 +1,5 @@
 //Adding Attribute For Contacts
-class Contact{ 
+class Contact {
     firstName
     lastName
     address
@@ -8,133 +8,147 @@ class Contact{
     zipCode
     email
     phoneNumber
-//Constructor to initialize variables
-    constructor(firstName,lastName,address,state,city,zipCode,email,phoneNumber){
-        this.firstname=firstName
-        this.lastname=lastName
-        this.address=address
-        this.state=state
-        this.city=city
-        this.zipcode=zipCode
-        this.email=email
-        this.phonenumber=phoneNumber
-    }   
+    //Constructor to initialize variables
+    constructor(firstName, lastName, address, state, city, zipCode, email, phoneNumber) {
+        this.firstname = firstName
+        this.lastname = lastName
+        this.address = address
+        this.state = state
+        this.city = city
+        this.zipcode = zipCode
+        this.email = email
+        this.phonenumber = phoneNumber
+    }
 }
 /*Validating Regex pattern for Every Attribute in Contact*/
 
-function contactDetails(firstName,lastName,address,state,city,zipCode,email,phoneNumber){
-    const firstNamePattern = /^[A-Z][a-zA-Z]{3,}/;
-    let firstName_Check = firstNamePattern.test(firstName);
+function contactDetails(firstName, lastName, address, state, city, zipCode, email, phoneNumber) {
 
-    const lastNamePattern = /^[A-Z][a-zA-Z]{3,}/;
-    let lastName_Check = lastNamePattern.test(lastName);
+    /*checking for duplicate contacts*/
+    addressBook.filter(contact => contact.firstName == firstName)
+        .reduce(() => count++, count = 0);
+    if (count > 0) {
+        console.log("Contact With Name has a duplicate contact & " + firstName + " Already Present")
+    } else {
+        const firstNamePattern = /^[A-Z][a-zA-Z]{3,}/;
+        let firstName_Check = firstNamePattern.test(firstName);
 
-    const addressPattern = /^[A-Z][a-zA-Z0-9]{3,}/;
-    let address_Check = addressPattern.test(address);
+        const lastNamePattern = /^[A-Z][a-zA-Z]{3,}/;
+        let lastName_Check = lastNamePattern.test(lastName);
 
-    const statePattern = /^[A-Z][a-zA-Z]{3,}/;
-    let state_Check = statePattern.test(state);
+        const addressPattern = /^[A-Z][a-zA-Z0-9]{3,}/;
+        let address_Check = addressPattern.test(address);
 
-    const cityPattern = /^[A-Z][a-zA-Z]{3,}/;
-    let city_Check = cityPattern.test(city);
+        const statePattern = /^[A-Z][a-zA-Z]{3,}/;
+        let state_Check = statePattern.test(state);
 
-    const zipCodePattern = /^[0-9]{3}[\s]{1}[0-9]{3}/;
-    let zipCode_Check = zipCodePattern.test(zipCode) ;
+        const cityPattern = /^[A-Z][a-zA-Z]{3,}/;
+        let city_Check = cityPattern.test(city);
 
-    const emailPattern = /^[A-Za-z0-9]+(.[A-Za-z0-9]+)*@[^_\\W]+(.[^_\\W]+)?(?=(.[^_\\W]{3,}$|.[a-zA-Z]{2}$)).*$/;
-    let email_Check = emailPattern.test(email);
+        const zipCodePattern = /^[0-9]{3}[\s]{1}[0-9]{3}/;
+        let zipCode_Check = zipCodePattern.test(zipCode);
 
-    const phoneNumberPattern = /^[0-9]{2}[\s]{1}[0-9]{10}/;
-    let phoneNumber_Check = phoneNumberPattern.test(phoneNumber);
+        const emailPattern = /^[A-Za-z0-9]+(.[A-Za-z0-9]+)*@[^_\\W]+(.[^_\\W]+)?(?=(.[^_\\W]{3,}$|.[a-zA-Z]{2}$)).*$/;
+        let email_Check = emailPattern.test(email);
 
-        if(firstName_Check == true && lastName_Check == true && address_Check == true && state_Check == true && city_Check == true
-            && zipCode_Check == true && email_Check == true && phoneNumber_Check == true){
-            let newCotact = new Contact(firstName,lastName,address,state,city,zipCode,email,phoneNumber);
+        const phoneNumberPattern = /^[0-9]{2}[\s]{1}[0-9]{10}/;
+        let phoneNumber_Check = phoneNumberPattern.test(phoneNumber);
+
+        if (firstName_Check == true && lastName_Check == true && address_Check == true && state_Check == true && city_Check == true
+            && zipCode_Check == true && email_Check == true && phoneNumber_Check == true) {
+            let newCotact = new Contact(firstName, lastName, address, state, city, zipCode, email, phoneNumber);
             console.log("Contact Added Successfully");
             console.log(newCotact);
-        }else{
+        } else {
             throw 'Contact Details Are Invalid ! please check details';
-        }
-}
-/*Function to edit Contact*/
-
-var addressBook = new Array();
-
-function editContact(findName,editedVariable,variableNewValue){
-    addressBook.forEach(newContact => {
-        if(newContact.firstName == findName){
-            switch(editedVariable){
-                case "firstName":
-                    newContact.firstName = variableNewValue;
-                    break;
-                case "lastName":
-                    newContact.lastName = variableNewValue;
-                    break;
-                case "address":
-                    newContact.address = variableNewValue;
-                    break;
-                case "state":
-                    newContact.state = variableNewValue;
-                    break;
-                case "city":
-                    newContact.city = variableNewValue;
-                    break;
-                case "zipCode":
-                    newContact.zipCode = variableNewValue;
-                    break;  
-                case "firstName":
-                    newContact.firstName = variableNewValue;
-                    break;
-                case "lastName":
-                    newContact.lastName = variableNewValue;
-                    break;      
-            }
-        }
-    })
-}
-function deleteContact(first_Name){
-    if(addressBook.length == null){
-        console.log("Contact Added In Address Book");
-    }else{
-        for(let i = 0; i <addressBook.length ; i++){
-            if(addressBook[i].firstName == first_Name){
-                addressBook.splice(i,1);
-                console.log("Contact Deleted from Addressbook");
-            }
         }
     }
 }
+    /*Function to edit Contact*/
 
-function selectFunction(select){
-    switch(select){
-        case "contactDetails":
+    var addressBook = new Array();
+
+    function editContact(findName, editedVariable, variableNewValue) {
+        addressBook.forEach(newContact => {
+            if (newContact.firstName == findName) {
+                switch (editedVariable) {
+                    case "firstName":
+                        newContact.firstName = variableNewValue;
+                        break;
+                    case "lastName":
+                        newContact.lastName = variableNewValue;
+                        break;
+                    case "address":
+                        newContact.address = variableNewValue;
+                        break;
+                    case "state":
+                        newContact.state = variableNewValue;
+                        break;
+                    case "city":
+                        newContact.city = variableNewValue;
+                        break;
+                    case "zipCode":
+                        newContact.zipCode = variableNewValue;
+                        break;
+                    case "firstName":
+                        newContact.firstName = variableNewValue;
+                        break;
+                    case "lastName":
+                        newContact.lastName = variableNewValue;
+                        break;
+                }
+            }
+        })
+    }
+    function deleteContact(first_Name) {
+        if (addressBook.length == null) {
+            console.log("Contact Added In Address Book");
+        } else {
+            for (let i = 0; i < addressBook.length; i++) {
+                if (addressBook[i].firstName == first_Name) {
+                    addressBook.splice(i, 1);
+                    console.log("Contact Deleted from Addressbook");
+                }
+            }
+        }
+    }
+
+    function selectFunction(select) {
+        switch (select) {
+            case "contactDetails":
                 //Calling Contact Details Fucntion
-                contactDetails("Nikhil","Patil","Nagpur","Maharashtra","Nagpur","444444","nikhil@gmail.com","91 1234567890");
-                contactDetails("Adesh","Jain","Aurangabad","Maharashtra","Aurangabad","444444","adijain@gmail.com","91 1098755454");
+                contactDetails("Nikhil", "Patil", "Nagpur", "Maharashtra", "Nagpur", "444444", "nikhil@gmail.com", "91 1234567890");
+                contactDetails("Adesh", "Jain", "Aurangabad", "Maharashtra", "Aurangabad", "444444", "adijain@gmail.com", "91 1098755454");
+                contactDetails("Nikhil", "Patil", "Nagpur", "Maharashtra", "Nagpur", "444444", "nikhil@gmail.com", "91 1234567890");
+
                 console.log(addressBook);
                 break;
-        case "editContact":
+            case "editContact":
                 //Calling Edit Contact Function
-                editContact("Nikihl","City","Nagpur");
+                editContact("Nikihl", "City", "Nagpur");
                 console.log(addressBook);
                 break;
-        case "deleteContact":
+            case "deleteContact":
                 //Calling Delete Contact Function
                 deleteContact("Adesh");
                 console.log(addressBook);
                 break;
+            case "countContact":
+                countContact();
+                break;
+        }
     }
-}
-function countContact(){
-    addressBook.reduce(() => {
-        count++;
-    },count = 0);
-    console.log("\nTotal Contacts In Address Book Are: "+count + "\n");
-}
-/*selecting a function from function*/
+    function countContact() {
+        addressBook.reduce(() => {
+            count++;
+        }, count = 0);
+        console.log("\nTotal Contacts In Address Book Are: " + count + "\n");
+    }
 
-selectFunction("contactDetails");
-selectFunction("countContact");
-selectFunction("editContact");
-selectFunction("deleteContact");
-selectFunction("countContact");
+    /*selecting a function from function*/
+
+    selectFunction("contactDetails");
+    selectFunction("countContact");
+    selectFunction("editContact");
+    selectFunction("deleteContact");
+    selectFunction("countContact");
